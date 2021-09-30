6893fde4774e25c5d82363030f2666458247701b
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:12:33 +0800

    Create practice_0206.py

---
+++ practice_0206.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 10:22:05 2021
+
+@author: fennieliang
+"""
+
+dic1 = {"orange":1,"melon":2,"apple":3}
+
+    
+for key, value in dic1.items():
+    if value == 2 :
+            print(key)
+        
+        
+    

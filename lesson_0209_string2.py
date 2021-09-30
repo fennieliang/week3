d6b2a145df8b469c0083f378fb1159a822e4a6d6
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:12:50 +0800

    Create lesson_0209_string2.py

---
+++ lesson_0209_string2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 11:00:43 2021
+
+@author: fennieliang
+"""
+#sub string
+string = input ('input a string:') #my name is xxxxx
+print (f"the string length: {len(string)}")
+print ("\n")
+
+
+substring = string[2:10] 
+print (f"the sub string is: {substring}")
+print ("\n")
+
+'''
+practice 
+0207: how to prevent error if your input string length is shorter than 5
+
+class_02: practice to create a class: Text_process
+          with a function: doc_length
+
+'''  

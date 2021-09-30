53f056549c4388eb9cb2fe5d6df2ffeba279e17f
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:12:30 +0800

    Create practice_0207.py

---
+++ practice_0207.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 14:12:11 2021
+
+@author: fennieliang
+"""
+
+string = input ('input a string:') #my name is xxxxx
+while len(string)<5:
+    string = input ('input a string:')
+    
+print (f"the string length: {len(string)}")
+print ("\n")
+
